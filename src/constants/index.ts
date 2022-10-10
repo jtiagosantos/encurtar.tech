6d4a47f3aedfe34c421bcf6271dbcabc9226f955
jtiagosantos <tiagosan040@gmail.com>
2022-10-10 13:21:49 -0300

    feat: create redis database instance

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,5 @@
 export const SHORTENER_API_URL = process.env
   .NEXT_PUBLIC_SHORTENER_API_URL as string;
+
+export const UPSTASH_REDIS_URL = process.env
+  .NEXT_PUBLIC_UPSTASH_REDIS_URL as string;
